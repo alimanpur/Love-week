702ab1df83b2a35036f30cbe4c712643a006aa3e
alimanpur <aliasgerm71@gmail.com>
2026-02-06 20:45:03 +0530

    add MusicToggle component to App for enhanced user interaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import GlobalBackground from "./components/GlobalBackground.jsx";
 import LoveWeek from "./pages/LoveWeek.jsx";
+import MusicToggle from "./components/MusicToggle.jsx";
 
 export default function App() {
   return (
     <div className="min-h-screen relative">
       {/* Full Page Animated Background */}
       <GlobalBackground />
+      <MusicToggle />
 
       {/* Main Love Week Timeline */}
       <LoveWeek />
